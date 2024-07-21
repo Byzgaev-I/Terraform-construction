e2e4b4058caf54c0db0c2b5095a53eb001229ecd
Byzgaev <110855219+Byzgaev-I@users.noreply.github.com>
2024-07-21 05:29:00 +0300

    Update count-vm.tf

--- src/count-vm.tf
+++ src/count-vm.tf
@@ -1,79 +1,38 @@
-variable "os_image_web" {
-  type    = string
-  default = "ubuntu-2004-lts"
+provider "yandex" {
+  token     = "y0_AgAAAAAAqYCBAATuwQAAAAEKezjYAADtzaLyKlNM7Z0e4gbif4KWCNHUjw"
+  cloud_id  = "b1g31ab21b32dog1ps4c"
+  folder_id = "b1gam4o6rj97es4peaq4"
 }
-
-data "yandex_compute_image" "ubuntu-2004-lts" {
-  family = var.os_image_web
-}
-variable "yandex_compute_instance_web" {
-  type        = list(object({
-    vm_name = string
-    cores = number
-    memory = number
-    core_fraction = number
-    count_vms = number
-    platform_id = string
-  }))
-
-  default = [{
-      vm_name = "web"
-      cores         = 2
-      memory        = 1
-      core_fraction = 5
-      count_vms = 2
-      platform_id = "standard-v1"
-    }]
-}
-
-variable "boot_disk_web" {
-  type        = list(object({
-    size = number
-    type = string
-    }))
-    default = [ {
-    size = 5
-    type = "network-hdd"
-  }]
-}
-
 
 resource "yandex_compute_instance" "web" {
-  name        = "${var.yandex_compute_instance_web[0].vm_name}-${count.index+1}"
-  platform_id = var.yandex_compute_instance_web[0].platform_id
+  count = 2
 
-  count = var.yandex_compute_instance_web[0].count_vms
+  name = "web-${count.index + 1}"
+
+  depends_on = [yandex_compute_instance.db]
 
   resources {
-    cores         = var.yandex_compute_instance_web[0].cores
-    memory        = var.yandex_compute_instance_web[0].memory
-    core_fraction = var.yandex_compute_instance_web[0].core_fraction
+    cores  = 2
+    memory = 2
   }
-
-
 
   boot_disk {
     initialize_params {
-      image_id = data.yandex_compute_image.ubuntu-2004-lts.image_id
-      type     = var.boot_disk_web[0].type
-      size     = var.boot_disk_web[0].size
+      image_id = "fd8rj2ffqf7h9k0r9m9g"  # Замените на ID вашего образа
     }
   }
 
-  metadata = {
-    ssh-keys = "ubuntu:${local.ssh-keys}"
-    serial-port-enable = "1"
+  network_interface {
+    subnet_id          = "<your-subnet-id>"
+    nat                = true
+    security_group_ids = ["<your-security-group-id>"]
   }
 
-  network_interface {
-    subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
-    security_group_ids = [
-      yandex_vpc_security_group.example.id
-    ]
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
   }
+
   scheduling_policy {
     preemptible = true
   }
-}
 }
