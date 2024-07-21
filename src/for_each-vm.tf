5dfe457259055e870c967a4b73e1af2a8b2f7747
Byzgaev <110855219+Byzgaev-I@users.noreply.github.com>
2024-07-21 05:38:45 +0300

    Update for_each-vm.tf

--- src/for_each-vm.tf
+++ src/for_each-vm.tf
@@ -1,66 +1,35 @@
-provider "yandex" {
-  token     = "y0_AgAAAAAAqYCBAATuwQAAAAEKezjYAADtzaLyKlNM7Z0e4gbif4KWCNHUjw"
-  cloud_id  = "b1g31ab21b32dog1ps4c"
-  folder_id = "b1gam4o6rj97es4peaq4"
+data "yandex_compute_image" "backend" {
+  family = var.backend_image_id
 }
 
-data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
-}
-
-variable "each_vm" {
-  type = list(object({
-    vm_name    = string
-    cpu        = number
-    ram        = number
-    disk_volume = number
-  }))
-}
-
-locals {
-  each_vm = [
-    {
-      vm_name    = "main"
-      cpu        = 2
-      ram        = 4
-      disk_volume = 50
-    },
-    {
-      vm_name    = "replica"
-      cpu        = 1
-      ram        = 2
-      disk_volume = 25
-    }
-  ]
-}
-
-resource "yandex_compute_instance" "db" {
-  for_each = { for vm in local.each_vm : vm.vm_name => vm }
-
-  name = each.value.vm_name
+resource "yandex_compute_instance" "backend" {
+  depends_on  = [yandex_compute_instance.web]
+  for_each    = { for i in var.backend_vm : i.name => i }
+  name        = each.value.name
+  platform_id = each.value.platform_id
 
   resources {
-    cores  = each.value.cpu
-    memory = each.value.ram
+    cores         = each.value.resources.cores
+    memory        = each.value.resources.memory
+    core_fraction = each.value.resources.core_fraction
   }
 
   boot_disk {
     initialize_params {
-      image_id = data.yandex_compute_image.ubuntu.id
-      size     = each.value.disk_volume
+      image_id = data.yandex_compute_image.backend.image_id
     }
   }
 
   network_interface {
-    subnet_id = "<your-subnet-id>"
-    nat       = true
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = each.value.nat
+    security_group_ids = [
+      yandex_vpc_security_group.example.id
+    ]
   }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-  }
-
   scheduling_policy {
-    preemptible = true
+    preemptible = each.value.preemptible
   }
+
+  metadata = merge(each.value.metadata, local.metadata)
 }
